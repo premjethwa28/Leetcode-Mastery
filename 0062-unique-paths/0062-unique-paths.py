363301927ce2d3e6fd3b40541e314a3c4105628c
Prem Atul Jethwa <109408402+premjethwa28@users.noreply.github.com>
2024-07-20 18:00:12 -0500

    Time: 36 ms (53.65%), Space: 16.7 MB (13.12%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,8 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        N = n+m-2
+        r = m-1
+        res = 1
+        for i in range(1, r+1):
+            res = res * (N-r+i)/i
+        return int(res)
